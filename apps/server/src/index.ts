02e4e4d3f80ae02f3d54dbad6e00ddfbe1e8cd80
Alon Pinto <pintoalon@gmail.com>
2023-12-06 15:49:37 +0200

    Config default route using express

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,22 @@
-console.log("ts project working");
+import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  return res.send("hello world");
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`==========================================`);
+  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`==========================================`);
+});
