5da4b458a3266910843154c64bdd2bd72a29ddbb
Alon Pinto <pintoalon@gmail.com>
2023-12-06 18:44:28 +0200

    Show hint after (x) seconds

--- apps/web/src/components/QuizQuestion/QuizQuestion.tsx
+++ apps/web/src/components/QuizQuestion/QuizQuestion.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { QuestionClientDto } from "../../dtos/question.dto";
+import QuizSetting from "../../settings/quiz.settings";
 import QuizOption from "../QuizOption/QuizOption";
 
 interface QuizQuestionProps {
@@ -6,20 +8,47 @@ interface QuizQuestionProps {
   handleNextQuestion: () => void;
 }
 
-const QuizQuestion = ({ question, handleNextQuestion }: QuizQuestionProps) => (
-  <>
-    <div>{question.question}</div>
+const QuizQuestion = ({ question, handleNextQuestion }: QuizQuestionProps) => {
+  const [hint, setHint] = useState<string | undefined>(undefined);
 
-    <div>
-      {question.choices.map((option) => (
-        <QuizOption option={option} />
-      ))}
-    </div>
+  const showHint = () => {
+    setHint(question.hint);
+  };
 
-    <div>
-      <input type="button" onClick={handleNextQuestion} value="Next Question" />
-    </div>
-  </>
-);
+  useEffect(() => {
+    // console.log(`question`, data.question);
+    setHint(undefined);
+    const showHintTimer = setTimeout(
+      showHint,
+      QuizSetting.showHintTimeInSeconds
+    );
+
+    return () => {
+      clearTimeout(showHintTimer);
+    };
+  }, []);
+
+  const onNextQuestion = () => {
+    setHint(undefined);
+    handleNextQuestion();
+  };
+
+  return (
+    <>
+      <div>{question.question}</div>
+
+      <div>
+        {question.choices.map((option) => (
+          <QuizOption option={option} />
+        ))}
+      </div>
+
+      <div>
+        <input type="button" onClick={onNextQuestion} value="Next Question" />
+      </div>
+      {hint && <div>Hint:{hint}</div>}
+    </>
+  );
+};
 
 export default QuizQuestion;
