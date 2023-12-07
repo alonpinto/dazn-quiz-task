a1d31acacfef4ec91b34c2923ce8ce1166247c54
Alon Pinto <pintoalon@gmail.com>
2023-12-07 08:50:48 +0200

    - set game setting according requirements

--- apps/web/src/settings/quiz.settings.ts
+++ apps/web/src/settings/quiz.settings.ts
@@ -1,7 +1,7 @@
 const QuizSetting = {
-  answerSessionTimeInSeconds: 10 * 1000,
-  showHintTimeInSeconds: 6 * 1000,
-  moveToNextQuestionAfterGuess: 500,
+  answerSessionTimeInSeconds: 20 * 1000,
+  showHintTimeInSeconds: 10 * 1000,
+  moveToNextQuestionAfterGuess: 1000,
   numberOfQuestions: 5,
 };
 export default QuizSetting;
